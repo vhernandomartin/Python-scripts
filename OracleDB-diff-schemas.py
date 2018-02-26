96ee5f4c3283cbf812b647738bfca41c88c64c06
victor <vhernandomartin@gmail.com>
2018-02-26 16:03:09 +0100

    v2

--- OracleDB-diff-schemas.py
+++ OracleDB-diff-schemas.py
@@ -4,6 +4,8 @@ import difflib
 import getopt
 import sys
 import cx_Oracle
+from termcolor import colored
+from termcolor import cprint
 
 def printf (format,*args):
     sys.stdout.write (format % args)
@@ -55,6 +57,17 @@ class DBConnection:
         #for object_name in cursor.fetchall():
         #    myPrintf(" %s;\n",object_name)
 
+    def sql_get_ddl(self, cursor, owner, object_type, object_name):
+        sql = "select dbms_metadata.get_ddl('" + object_type + "','" + object_name + "','" + owner + "') from dual"
+        try:
+            cursor.execute (sql)
+        except cx_Oracle.DatabaseError, exception:
+            myPrintf ('Failed to prepare cursor\n')
+            myprintException (exception)
+            exit (1)
+        ddl_object = cursor.fetchall()
+        return ddl_object
+
     def close_cursor(self, cursor):
         cursor.close ()
 
@@ -68,17 +81,75 @@ def get_all_objects(con,diff_user1,diff_user2):
         print '#### ' + db_object_type + ' ####'
         objects_output = {}
         i = 0
+        # Get object list from each schema
         for db_user in [diff_user1,diff_user2]:
             #print '  \__ ' + db_user
             object_list = con.sql_get_object_name(cursor,'OBJECT_NAME',db_user,db_object_type)
             objects_output[i] = object_list
             i += 1
-        diff_part1 = "\n".join(str(e) for e in objects_output[0]).replace("(","").replace(")","")
-        diff_part2 = "\n".join(str(e) for e in objects_output[1]).replace("(","").replace(")","")
+        # Get diff from two strings
+        objects_output1 = objects_output[0]
+        objects_output2 = objects_output[1]
+        diff_part1 = "\n".join(str(e) for e in objects_output1).replace("(","").replace(")","")
+        diff_part2 = "\n".join(str(e) for e in objects_output2).replace("(","").replace(")","")
         diff_part1 = diff_part1.splitlines(1)
         diff_part2 = diff_part2.splitlines(1)
         diff = difflib.unified_diff(diff_part1,diff_part2, n=0)
-        print (''.join(diff))
+        diff_output1 = (''.join(diff))
+        if diff_output1 == '':
+            print "INFO: Same number of " + db_object_type + " found"
+        else:
+            cprint("WARNING!!",attrs=['bold'])
+            print "     \_ Has been detected " + colored(db_object_type,'red') + " objects are not present in one of the schemas!!"
+            print diff_output1
+        print "INFO: Checking differences on " + db_object_type + " objects between schemas...\n"
+
+        object_matches = [c for c in objects_output2 if c in objects_output1]
+        for object in object_matches:
+            # Get DDLs for each OBJECT_TYPE
+            if db_object_type == 'QUEUE':
+                ddl_object_type = "AQ_QUEUE"
+            elif db_object_type == 'PACKAGE BODY':
+                ddl_object_type = "PACKAGE_BODY"
+            elif db_object_type == 'MATERIALIZED VIEW':
+                ddl_object_type = "MATERIALIZED_VIEW"
+            elif db_object_type == 'TABLE PARTITION':
+                break
+            elif db_object_type == 'DATABASE LINK':
+                break
+            elif db_object_type == 'LOB':
+                break
+            elif db_object_type == 'JOB':
+                break
+            elif db_object_type == 'SEQUENCE':
+                break
+            else:
+                ddl_object_type = db_object_type
+            ddl_metadata = {}
+            j = 0
+            for db_user in [diff_user1,diff_user2]:
+                #print ddl_object_type
+                object = str(object)
+                object = object.replace(",","").replace("'","").replace("(","").replace(")","")
+                ddl_object = con.sql_get_ddl(cursor,db_user,ddl_object_type,object)
+                print_ddl = ddl_object[0][0]
+                ddl_metadata[j] = print_ddl
+                j += 1
+                #print print_ddl.read()
+            ddl_metadata1 = str(ddl_metadata[0])
+            ddl_metadata2 = str(ddl_metadata[1])
+            diff_part1 = "".join(str(e) for e in ddl_metadata1).replace(diff_user1,"")
+            diff_part2 = "".join(str(e) for e in ddl_metadata2).replace(diff_user2,"")
+            diff_part1 = diff_part1.splitlines(1)
+            diff_part2 = diff_part2.splitlines(1)
+            diff = difflib.unified_diff(diff_part1,diff_part2,n=0)
+            diff_output2 = (''.join(diff))
+            if diff_output2 == '':
+                print "|_"
+                print " \->INFO: " + object + " No differences found"
+            else:
+                print ('\033[1m{:10s}\033[0m'.format('WARNING-->')) + colored(object,'red') + " Check the differences between objects!!"
+            print diff_output2
 
 def main():
     try:
